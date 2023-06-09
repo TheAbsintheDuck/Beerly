31e3e70276569763dc47d2eb7488f5991e648f15
TheAbsintheDuck <112381607+TheAbsintheDuck@users.noreply.github.com>
2023-06-09 13:38:57 +0200

    sec

---
+++ wwwroot/js/fake-fetch.js
@@ -0,0 +1,115 @@
+﻿export default function fakeFetch(urlString, options) {
+    const url = new URL(urlString);
+
+
+    if (url.hostname === 'pizzaproject3.azurewebsites.net' && url.pathname === '/api') {
+
+        //Jag har bara behållt det här så länge. Den kommer aldrig att köras eftersom q aldrig= animal
+        if (url.searchParams.get('q') === 'animal') {
+            return {
+                total: 4,
+                totalHits: 4,
+                hits: [
+                    {
+                        webformatURL: '/fake-images/redpanda.jpg'
+                    },
+                    {
+                        webformatURL: '/fake-images/koala.jpg'
+                    },
+                    {
+                        webformatURL: '/fake-images/panda.jpg'
+                    },
+                    {
+                        webformatURL: '/fake-images/raccoon.jpg'
+                    }
+                ]
+            }
+        }
+
+        // Har lagt lite förenklad hämtning här. Om Vegetarian finns i GoesWellWith så kommer bild på vegetarisk pizza alltid att visas 
+        //(även om GoesWellWith även inkluderar Meat). Kanske inte optimalt men funkar. 
+        //Skulle kunna vara bra att lägga in någon sorts randomizer, men gör inte det nu. 
+        //Tänkte inte greja mer med detta nu förrän vi har ett riktigt API att fetcha
+
+        else if (url.searchParams.get('q').includes('Vegetarian')) {
+            return {
+                total: 1,
+                totalHits: 1,
+                hits: [
+                    {
+                        webformatURL: '/fake-images/Vegetarian.jpg',
+                        pizzaName: 'Veggie Pizza',
+                        ingredients: 'tomato, cheese, olives'
+                    }
+                ]
+            }
+        }
+
+        else if (url.searchParams.get('q').includes('Meat')) {
+            return {
+                total: 1,
+                totalHits: 1,
+                hits: [
+                    {
+                        webformatURL: '/fake-images/Meat.jpg',
+                        pizzaName: 'Meat Pizza',
+                        ingredients: 'tomato, cheese, ham'
+                    }
+                ]
+            }
+        }
+
+        else if (url.searchParams.get('q').includes('Chicken')) {
+            return {
+                total: 1,
+                totalHits: 1,
+                hits: [
+                    {
+                        webformatURL: '/fake-images/Chicken.jpg',
+                        pizzaName: 'Chicken Pizza',
+                        ingredients: 'tomato, cheese, chicken'
+                    }
+                ]
+            }
+        }
+        else if (url.searchParams.get('q').includes('Dessert')) {
+            return {
+                total: 1,
+                totalHits: 1,
+                hits: [
+                    {
+                        webformatURL: '/fake-images/Dessert.jpg',
+                        pizzaName: 'Dessert Pizza',
+                        ingredients: 'nutella, banana'
+                    }
+                ]
+            }
+        }
+        else if (url.searchParams.get('q').includes('Fish')) {
+            return {
+                total: 1,
+                totalHits: 1,
+                hits: [
+                    {
+                        webformatURL: '/fake-images/Fish.jpg',
+                        pizzaName: 'Fish Pizza',
+                        ingredients: 'tomato, cheese, tuna'
+
+                    }
+                ]
+            }
+        }
+
+        else {
+            return {
+                total: 0,
+                totalHits: 0,
+                hits: []
+            }
+
+        }
+    }
+    else {
+        throw new Error('This URL has not been implemented in the fake API: ' + urlString);
+    }
+}
---
+++ wwwroot/js/pizza-search.js
@@ -0,0 +1,175 @@
+﻿import fakeFetch from './fake-fetch.js';
+
+const useRealAPI = true;
+
+async function fetchJSON(url, options) {
+    if (useRealAPI) {
+            const response = await fetch(url, options);
+            const json = await response.json();
+            return json;
+    }
+    else {
+        // Sleep for one second to simulate a network delay.
+        await new Promise((r) => setTimeout(r, 1000));
+        const json = fakeFetch(url, options);
+        return json;
+    }
+}
+
+//Event-listener DOM, to make sure page is loaded. Don't know if needed
+document.addEventListener('DOMContentLoaded', () => {
+    const goesWithFoodElement = document.querySelector('#goesWithFood');
+    const message = document.querySelector('#message');
+    const resultList = document.querySelector('#recommendation');
+
+    function displayResults(result) {
+        if (result.length === 0) {
+            message.hidden = false;
+            resultList.hidden = true;
+        } else {
+            message.hidden = true;
+            resultList.hidden = false;
+
+            resultList.replaceChildren();
+
+          /*  for (const hit of hits) {*/
+                const pizzaName = document.createElement('h4');
+                pizzaName.setAttribute("class", "pizzaName");
+
+                const img = document.createElement('img');
+
+                const ingredientHeader = document.createElement('h4');
+                ingredientHeader.textContent = 'Ingredients'
+
+                const ingredientList = document.createElement('ul');
+                ingredientList.setAttribute("class", "ingredientList");
+
+                /*const ingredientString = hit.ingredients;*/
+
+                //retrieve the string from the API response and split into an array
+                var ingredientsArray = result.ingredients;
+
+                // Iterate over the array of words
+                for (var i = 0; i < ingredientsArray.length; i++) {
+                    // Trim each word to remove any leading or trailing spaces
+                    var ingredient = ingredientsArray[i].trim();
+
+                    // Create an li element for each word
+                    var ingredientLi = document.createElement('li');
+
+                    // Set the text content of the li element to the ingredient
+                    ingredientLi.textContent = ingredient;
+
+                    // Append the li element to the ul element
+                    ingredientList.appendChild(ingredientLi);
+                }
+
+                img.src = result.photo.toString();
+                img.style.width = '250px';
+             
+                pizzaName.textContent = result.name;
+
+                const li = document.createElement('li');
+                li.append(pizzaName);
+                li.append(img);
+                li.append(ingredientHeader);
+                li.append(ingredientList);
+
+                resultList.append(li);
+        }
+    }
+
+    async function searchPizzas(categoryname) {
+        try { 
+        const result = await fetchJSON(
+            'https://pizzaproject3.azurewebsites.net/api?' + new URLSearchParams({ categoryname }).toString()
+        );
+
+        displayResults(result);
+        }
+          
+        catch {
+            const message = document.querySelector('#message');
+            message.hidden = false;
+        }
+
+    }
+
+    // When the page is loaded, fetch data from the API.
+    const string = goesWithFoodElement.textContent.trim();
+    const trimmedString = string.split(',')[0];
+    searchPizzas(trimmedString);
+
+
+    // When the form is submitted, fetch data from the API.
+
+    /*const form = document.querySelector('#pizza-form');*/
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const pizzacategory = formData.get('category');
+        searchPizzas(pizzacategory);
+    });
+});
+
+
+//import fakeFetch from './fake-fetch.js';
+
+
+//const useRealAPI = false;
+
+//async function fetchJSON(url, options) {
+//    if (useRealAPI) {
+//        const response = await fetch(url, options);
+//        const json = await response.json();
+//        return json;
+//    }
+//    else {
+//        // Sleep for one second to simulate a network delay.
+//        await new Promise(r => setTimeout(r, 1000));
+//        const json = fakeFetch(url, options);
+//        return json;
+//    }
+//}
+
+
+//const goesWithFoodElement = document.querySelector('#goesWithFood');
+//const pizzacategory = goesWithFoodElement.textContent.trim();
+//const message = document.querySelector('#message');
+
+//const pizzadiv = document.querySelector('#pizzaGridCell')
+//var pizzaname = document.querySelector('#pizzaName')
+//var ingredientlist = document.querySelector('#ingredientlist')
+//var resultList = document.querySelector('#recommendation')
+
+
+//    const result = await fetchJSON(
+//        'https://pizzaexample.com/api/?' +
+//        new URLSearchParams({
+//            q: pizzacategory,
+//        }).toString()
+//    );
+
+//    if (result.hits.length === 0) {
+//        message.hidden = false;
+//        resultList.hidden = true;
+//    }
+//    else {
+//        message.hidden = true;
+//        resultList.hidden = false;
+
+//        resultList.replaceChildren();
+
+//        for (const hit of result.hits) {
+//            const img = document.createElement('img');
+//            img.src = hit.webformatURL;
+//            img.style.width = '250px';
+
+//            const li = document.createElement('li');
+//            li.append(img);
+
+//            resultList.append(li);
+//        }
+//    }
